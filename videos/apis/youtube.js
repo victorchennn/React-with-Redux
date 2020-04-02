fc81c29794f8e682c2f1b1c5df9b94851109af2d
Chen <victorchennn@gmail.com>
2020-04-02 13:31:56 -0400

    hide key

--- videos/apis/youtube.js
+++ videos/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAhL6WZP9erPiLUDTxAsgyW5wltL2jXXL8";
+const KEY = "...";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
